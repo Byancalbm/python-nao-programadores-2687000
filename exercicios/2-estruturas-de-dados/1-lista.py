b025f9180e7fb0e07ef9c43a91a9f0edfe8c0440
Byancalbm <130987371+Byancalbm@users.noreply.github.com>
2025-10-03 15:08:14 +0000

    resolução exercicios

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,21 @@
 # Crie uma lista apenas com elementos numéricos
+anos = [1983,1996,2017,2025]
 
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+familia = ['Marcio', 'Byanca', 'Lorenzo', 'Lorena', 42,29,8,1, 'Piracicaba']
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
+familia [0:5]
 
 # Crie um slice na lista para que imprima na tela os elementos de índice par
+familia [0:-1:2]
 
 # Remova da lista o último item
+familia.pop ()
 
 # Insira na lista um novo item
+familia.append('SP')
+familia.extend(anos)
 
 # Remova da lista um item específico
+familia.remove('Piracicaba')
