ec9e274d5c842309e53252ee22d95f30b8f134a7
Byancalbm <130987371+Byancalbm@users.noreply.github.com>
2025-10-03 16:02:13 +0000

    resolucão de exercicios

--- exercicios/2-estruturas-de-dados/2-dicionario.py
+++ exercicios/2-estruturas-de-dados/2-dicionario.py
@@ -7,12 +7,24 @@ pessoa = {'nome':'Crislaine',
           'animal_estimacao':False}
 
 # Imprima na tela o valor equivalente a chave "hobby"
-
+pessoa['hobby']
+print (pessoa['hobby'])
 
 # Imprima na tela uma lista apenas com os valores do dicionário
-
+pessoa.values ()
+list(pessoa.values())
+print (list (pessoa.values ()))
 
 # Imprima na tela uma lista apenas com as chaves do dicionário
-
+pessoa.keys()
+list(pessoa.keys())
+print (list(pessoa.keys()))
 
 # Insira um novo par chave-valor no dicionário
+pessoa['altura'] = 1.68
+
+# Altere um valor no dicionário
+pessoa['nome'] = 'Byanca'
+
+# Remova um valor no dicionário
+pessoa.pop ('idade')
--- exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
+++ exercicios/2-estruturas-de-dados/projeto-resumo_vida_estudantil.py
@@ -1,5 +1,20 @@
 # Nesse exercício coletaremos dados de uma estudante, armazenaremos em um dicionário e imprimiremos na tela esse dados em um formato amigável.
 
 # 1. Solicite a estudante os seguintes dados: nome, ano que conheceu o LinkedIn, ano atual e os cursos realizados no LinkedIn Learning separados por virgula em ordem cronológica
+estudante = {}
+
+estudante['nome'] = input('Qual seu nome?')
+estudante['ano_linkedin'] = int(input('Em qual ano você conheceu o Linkedin?'))
+estudante['ano_atual'] = int(input('Qual é o ano atual?'))
+cursos = input('Quais cursos você já fez no linkedin? (separe-os por virgula)')
+estudante ['cursos'] = cursos.split(', ')
+
 # 2. Armazene esses dados em um dicionário
+
+
 # 3. Imprima na tela uma string com as informações de nome, ano_conheceu_linkedin, total de anos transcurridos, total de cursos realizados e (apenas) o primeiro e último curso.
+
+total_anos = estudante['ano_atual'] - estudante['ano_linkedin']
+total_curso = len(estudante['cursos'])
+
+print(f"Olá {estudante['nome']}, você conhece o linkedind desde {estudante['ano_linkedin']}, nesses {total_anos} você realizou {total_curso} cursos, sendo o primeiro curso {estudante['cursos'][0]} e o ultimo curso foi o {estudante['cursos'][-1]}")
